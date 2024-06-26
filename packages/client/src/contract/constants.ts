dd543055cfc363782a25b8b8fee650801d492bff
BriefCandle <yufanling@gmail.com>
2024-06-27 00:41:06 +0800

    feat(client): add terrain constants

---
+++ packages/client/src/contract/constants.ts
@@ -0,0 +1,13 @@
+import { toHex } from "viem";
+
+export const TEST = toHex("testing", { size: 32 });
+
+export const GRASS = toHex("GRASS", { size: 16 });
+export const WATER = toHex("WATER", { size: 16 });
+export const BUSH = toHex("BUSH", { size: 16 });
+export const ROCK = toHex("ROCK", { size: 16 });
+export const TREE = toHex("TREE", { size: 16 });
+export const GRAVEL = toHex("GRAVEL", { size: 16 });
+export const SAND = toHex("SAND", { size: 16 });
+export const STUMP = toHex("STUMP", { size: 16 });
+export const RED_MINE = toHex("RED_MINE", { size: 16 });
