47b004caf37c9ad81825f76ec4bf845109661afb
BriefCandle <yufanling@gmail.com>
2024-06-26 15:37:35 +0800

    feat(contract): add MapLogic & Errors system

---
+++ packages/contracts/src/Errors.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { System } from "@latticexyz/world/src/System.sol";
+
+contract Errors is System {
+  // move error
+  error CannotMoveToTerrain(bytes16 terrainType);
+  error CannotMoveToBuilding(bytes32 coordId);
+  error ExceedMaxMoves();
+  error NotIncrementalMoves();
+  error NotFromHostPosition();
+}
---
+++ packages/contracts/src/libraries/MapLogic.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { TerrainSpecs, RemovedCoord, BuildingCoord, EntityType } from "@/codegen/index.sol";
+import { Perlin } from "../utils/Perlin.sol";
+import { Errors } from "@/Errors.sol";
+import "@/constants.sol";
+
+library MapLogic {
+  // noise is 20~80
+  function getPerlin(uint32 x, uint32 y) internal pure returns (uint8) {
+    int128 noise = Perlin.noise2d(int40(uint40(x)), int40(uint40(y)), 9, 64);
+
+    uint8 precision = 2;
+    uint256 denominator = 10 ** precision;
+
+    uint256 noise1 = (uint256(uint128(noise)) * denominator) >> 64;
+    return uint8(noise1);
+  }
+
+  function noiseToTerrainType(uint8 noise) internal pure returns (bytes16) {
+    if (noise < 23) return ROCK;
+    if (noise < 40) return WATER;
+    if (noise < 55) return GRASS;
+    if (noise < 65) return BUSH;
+    if (noise < 78) return TREE;
+    else return RED_MINE;
+  }
+
+  function getTerrainType(uint32 x, uint32 y) internal view returns (bytes16) {
+    uint8 noise = getPerlin(x, y);
+    bytes16 terrainType = noiseToTerrainType(noise);
+    bytes32 coordId = getCoordId(x, y);
+    if (RemovedCoord.get(coordId) && !TerrainSpecs.getCanMove(terrainType)) {
+      return GRASS;
+    }
+    return terrainType;
+  }
+
+  function canMoveTo(uint32 x, uint32 y) internal view returns (bool) {
+    bytes16 terrainType = getTerrainType(x, y);
+    bool terrainCanMove = TerrainSpecs.getCanMove(terrainType);
+    bytes32 coorId = getCoordId(x, y);
+    bool hasBuilding = BuildingCoord.get(coorId) == 0;
+    return terrainCanMove && hasBuilding;
+  }
+
+  function canMoveToStrict(uint32 x, uint32 y) internal view {
+    bytes16 terrainType = getTerrainType(x, y);
+    bool terrainCanMove = TerrainSpecs.getCanMove(terrainType);
+    if (!terrainCanMove) revert Errors.CannotMoveToTerrain(terrainType);
+    bytes32 coorId = getCoordId(x, y);
+    bool hasBuilding = BuildingCoord.get(coorId) == 0;
+    if (!hasBuilding) revert Errors.CannotMoveToBuilding(coorId);
+  }
+
+  function getCoordId(uint32 x, uint32 y) internal pure returns (bytes32) {
+    return bytes32((uint256(x) << 128) | uint256(y));
+  }
+}
