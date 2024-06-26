c264340b26695ca63e46e66df525cddaaa540b29
BriefCandle <yufanling@gmail.com>
2024-06-26 12:56:13 +0800

    chore(client): install perlin noise; update optimizer config in build time

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -9,7 +9,30 @@ export default defineConfig({
       strict: false,
     },
   },
+  optimizeDeps: {
+    exclude: ["@latticexyz/network", "@latticexyz/noise"],
+  },
   build: {
+    // outDir: "../dist",
+    rollupOptions: {
+      external: ["chalk"], // fix for chalk, necessary for successful build but dist fails on runtime
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          phaser: ["phaser"],
+          mud: [
+            "@latticexyz/common",
+            "@latticexyz/dev-tools",
+            "@latticexyz/react",
+            "@latticexyz/recs",
+            "@latticexyz/schema-type",
+            "@latticexyz/store-sync",
+            "@latticexyz/utils",
+            "@latticexyz/world",
+          ],
+        },
+      },
+    },
     target: "es2022",
     minify: true,
     sourcemap: true,
