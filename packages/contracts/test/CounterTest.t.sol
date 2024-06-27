917cedfb079f6013d2b85768636482f7e2ed3670
BriefCandle <yufanling@gmail.com>
2024-06-27 18:50:35 +0800

    refactor(contract): comment off counter test

--- packages/contracts/test/CounterTest.t.sol
+++ packages/contracts/test/CounterTest.t.sol
@@ -9,23 +9,21 @@ import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Counter } from "../src/codegen/index.sol";
 
 contract CounterTest is MudTest {
-  function testWorldExists() public {
-    uint256 codeSize;
-    address addr = worldAddress;
-    assembly {
-      codeSize := extcodesize(addr)
-    }
-    assertTrue(codeSize > 0);
-  }
-
-  function testCounter() public {
-    // Expect the counter to be 1 because it was incremented in the PostDeploy script.
-    uint32 counter = Counter.get();
-    assertEq(counter, 1);
-
-    // Expect the counter to be 2 after calling increment.
-    IWorld(worldAddress).app__increment();
-    counter = Counter.get();
-    assertEq(counter, 2);
-  }
+  // function testWorldExists() public {
+  //   uint256 codeSize;
+  //   address addr = worldAddress;
+  //   assembly {
+  //     codeSize := extcodesize(addr)
+  //   }
+  //   assertTrue(codeSize > 0);
+  // }
+  // function testCounter() public {
+  //   // Expect the counter to be 1 because it was incremented in the PostDeploy script.
+  //   uint32 counter = Counter.get();
+  //   assertEq(counter, 1);
+  //   // Expect the counter to be 2 after calling increment.
+  //   IWorld(worldAddress).app__increment();
+  //   counter = Counter.get();
+  //   assertEq(counter, 2);
+  // }
 }
