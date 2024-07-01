584136daa84b6242b8919f3b39afb7ea0945ca7a
BriefCandle <yufanling@gmail.com>
2024-07-02 03:29:23 +0800

    feat(client): add cost & award logic, similar to CostLogic & AwardLogic in contract

---
+++ packages/client/src/logics/cost.ts
@@ -0,0 +1,85 @@
+import { Hex } from "viem";
+import { ClientComponents } from "../mud/createClientComponents";
+import { encodeTypeEntity, splitBytes32 } from "../utils/encode";
+import { hasBalance, hasERC20Balance } from "./container";
+import { isPoolType } from "./entity";
+import { getPoolAmount, hasPoolBalance } from "./pool";
+import { Entity, getComponentValue } from "@latticexyz/recs";
+import { getPool } from "../contract/hashes";
+
+export const hasMintCosts = (
+  components: ClientComponents,
+  role: Hex,
+  mintType: Hex
+) => {
+  const costs = getMintCosts(components, mintType);
+  if (!costs) return false;
+  return hasCosts(components, role, costs as Hex[]);
+};
+
+export const getMintCosts = (components: ClientComponents, mintType: Hex) => {
+  const typeEntity = encodeTypeEntity(mintType) as Entity;
+  return getComponentValue(components.MintCosts, typeEntity)?.costs ?? [];
+};
+
+export const hasInteractCosts = (
+  components: ClientComponents,
+  role: Hex,
+  interactType: Hex
+) => {
+  const costs = getInteractCosts(components, interactType);
+  if (!costs) return false;
+  return hasCosts(components, role, costs as Hex[]);
+};
+
+export const getInteractCosts = (
+  components: ClientComponents,
+  interactType: Hex
+) => {
+  const typeEntity = encodeTypeEntity(interactType) as Entity;
+  return getComponentValue(components.InteractCosts, typeEntity)?.costs ?? [];
+};
+
+export const hasUpgradeCosts = (
+  components: ClientComponents,
+  role: Hex,
+  upgradeType: Hex
+) => {
+  const costs = getUpgradeCosts(components, upgradeType);
+  if (!costs) return false;
+  return hasCosts(components, role, costs as Hex[]);
+};
+
+export const getUpgradeCosts = (
+  components: ClientComponents,
+  upgradeType: Hex
+) => {
+  const typeEntity = encodeTypeEntity(upgradeType) as Entity;
+  return getComponentValue(components.UpgradeCosts, typeEntity)?.costs ?? [];
+};
+
+export const hasBurnCosts = (
+  components: ClientComponents,
+  role: Hex,
+  burnType: Hex
+) => {
+  const costs = getBurnCosts(components, burnType);
+  if (!costs) return false;
+  return hasCosts(components, role, costs as Hex[]);
+};
+
+export const getBurnCosts = (components: ClientComponents, burnType: Hex) => {
+  const typeEntity = encodeTypeEntity(burnType) as Entity;
+  return getComponentValue(components.BurnCosts, typeEntity)?.costs ?? [];
+};
+
+export const hasCosts = (
+  components: ClientComponents,
+  role: Hex,
+  costs: Hex[]
+) => {
+  const costsData = costs.map((cost) => splitBytes32(cost));
+  return costsData.every(({ type, amount }) =>
+    hasERC20Balance(components, role, type, BigInt(amount))
+  );
+};
