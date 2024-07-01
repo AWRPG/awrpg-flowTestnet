38cc01b76a2deee8d7541d187bd7451912f36062
BriefCandle <yufanling@gmail.com>
2024-07-02 03:33:12 +0800

    refactor(client): refactor combine & split to make size bytes32

--- packages/client/src/logics/move.ts
+++ packages/client/src/logics/move.ts
@@ -132,12 +132,12 @@ export function isOnMap(position: Vector) {
 
 // combine two number into one bigint
 export function combine(x: number, y: number) {
-  return ((BigInt(x) << 32n) | BigInt(y)).toString();
+  return ((BigInt(x) << 128n) | BigInt(y)).toString();
 }
 
 // split one bigint into two number
 export function split(xy: bigint) {
-  const x = Number(xy >> 32n);
+  const x = Number(xy >> 128n);
   const y = Number(xy & 0xffffffffn);
   return { x, y };
 }
