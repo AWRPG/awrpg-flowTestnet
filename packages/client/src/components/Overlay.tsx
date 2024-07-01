aaf3c660930cff210dd88f25521f6d8f1bd41247
BriefCandle <yufanling@gmail.com>
2024-07-02 03:46:43 +0800

    refactor(client): update Overlay to display all menu

--- packages/client/src/components/Overlay.tsx
+++ packages/client/src/components/Overlay.tsx
@@ -1,13 +1,32 @@
 import { useComponentValue, useEntityQuery } from "@latticexyz/react";
 import { useMUD } from "../MUDContext";
-import { MAIN_MENU, MENU, SELECTED, SOURCE, TARGET } from "../constants";
+import {
+  TERRAIN_BURN_MENU,
+  EXPLORE_MENU,
+  MAIN_MENU,
+  MENU,
+  SELECTED,
+  SOURCE,
+  TARGET,
+  TERRAIN_MENU,
+  TERRAIN_INTERACT_MENU,
+  ROLE_MENU,
+  BAG_MENU,
+} from "../constants";
 import { Has, HasValue } from "@latticexyz/recs";
 // import PoolBars from "./PoolBars";
 import MainMenu from "./menu/MainMenu";
+import ExploreMenu from "./menu/ExploreMenu";
+import TerrainMenu from "./menu/TerrainMenu";
+import TerrainBurnMenu from "./menu/TerrainBurnMenu";
+import Console from "./Console";
+import TerrainInteractMenu from "./menu/TerrainInteractMenu";
+import RoleMenu from "./menu/RoleMenu";
+import BagMenu from "./menu/BagMenu";
 
 export default function Overlay() {
   const {
-    components: { SelectedHost, Commander, SelectedEntity },
+    components: { SelectedHost, Commander, SelectedEntity, ConsoleMessage },
     network: { playerEntity },
   } = useMUD();
 
@@ -15,6 +34,8 @@ export default function Overlay() {
   const target = useComponentValue(SelectedHost, TARGET)?.value;
 
   const menu = useComponentValue(SelectedEntity, MENU)?.value;
+
+  const message = useComponentValue(ConsoleMessage, SOURCE)?.value;
 
   const playerHosts = useEntityQuery([
     HasValue(Commander, { value: playerEntity }),
@@ -26,6 +47,17 @@ export default function Overlay() {
       <div className="relative h-full">
         <div className="absolute pointer-events-auto top-2 right-2">
           {menu === MAIN_MENU && <MainMenu player={playerEntity} />}
+          {menu === ROLE_MENU && <RoleMenu />}
+          {menu === BAG_MENU && <BagMenu />}
+        </div>
+        <div className="absolute pointer-events-auto top-1/2 right-2">
+          {menu === EXPLORE_MENU && <ExploreMenu />}
+          {menu === TERRAIN_MENU && <TerrainMenu />}
+          {menu === TERRAIN_BURN_MENU && <TerrainBurnMenu />}
+          {menu === TERRAIN_INTERACT_MENU && <TerrainInteractMenu />}
+        </div>
+        <div className="absolute pointer-events-auto bottom-2 w-1/2 left-1/4 h-48">
+          {message && <Console message={message} />}
         </div>
         {/* {source && (
           <div className="absolute pointer-events-auto top-2 left-2">
