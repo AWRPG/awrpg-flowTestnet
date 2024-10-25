c4215c3ad230120d07a0993cb47417036a4d205d
BriefCandle <yufanling@gmail.com>
2024-10-26 04:48:44 +0800

    feat(client): add Transfer comp & load it in Host comp

--- packages/client/src/components/host/Host.tsx
+++ packages/client/src/components/host/Host.tsx
@@ -3,14 +3,37 @@ import { useMUD } from "../../MUDContext";
 import { isBuilding } from "../../logics/entity";
 import { Building } from "../building/Building";
 import { Role } from "./Role";
+import { useState } from "react";
+import { useComponentValue } from "@latticexyz/react";
+import { SOURCE } from "../../constants";
+import { Transfer } from "../building/Transfer";
 
 /**
  * display a host, either a building or a role
  */
 export function Host({ host }: { host: Entity }) {
   const { components } = useMUD();
+  const { SelectedHost } = components;
+  const sourceHost = useComponentValue(SelectedHost, SOURCE)?.value as Entity;
   const buildingType = isBuilding(components, host);
+  // switch between transfer from sourceHost or to sourceHost
+  const [toTransfer, setToTransfer] = useState(true);
+
   return (
-    <>{buildingType ? <Building building={host} /> : <Role role={host} />}</>
+    <>
+      {buildingType ? <Building building={host} /> : <Role role={host} />}
+      {sourceHost && host !== sourceHost && (
+        <>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+            onClick={() => setToTransfer(!toTransfer)}
+          >
+            {toTransfer ? "Switch Transfer" : "Switch Transfer"}
+          </button>
+          {toTransfer && <Transfer fromHost={sourceHost} toHost={host} />}
+          {!toTransfer && <Transfer fromHost={host} toHost={sourceHost} />}
+        </>
+      )}
+    </>
   );
 }
