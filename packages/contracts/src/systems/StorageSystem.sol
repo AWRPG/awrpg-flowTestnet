81cdc06bed682bb9b653a5b4766f45228ff23e35
BriefCandle <yufanling@gmail.com>
2024-10-26 04:37:30 +0800

    update(contract): fix host may have multiple positions in StorageSystem; add transferERC721()

--- packages/contracts/src/systems/StorageSystem.sol
+++ packages/contracts/src/systems/StorageSystem.sol
@@ -6,6 +6,7 @@ import { TypeCast } from "@/utils/TypeCast.sol";
 import { ContainerLogic } from "@/libraries/ContainerLogic.sol";
 import { AccessControl } from "@/extensions/AccessControl.sol";
 import { PositionControl } from "@/extensions/PositionControl.sol";
+import { BuildingLogic } from "@/libraries/BuildingLogic.sol";
 
 contract StorageSystem is System, AccessControl, PositionControl {
   using TypeCast for address;
@@ -13,12 +14,34 @@ contract StorageSystem is System, AccessControl, PositionControl {
   // role <-> building; role <-> role
   function transferERC20(
     bytes16 itemType,
-    bytes32 from,
-    bytes32 to,
+    uint32 fromX,
+    uint32 fromY,
+    uint32 toX,
+    uint32 toY,
     uint128 amount
-  ) public onlyController(from) onlyAdjacentHosts(from, to) {
+  ) public onlyAdjacentCoords(fromX, fromY, toX, toY) {
+    bytes32 from = BuildingLogic.getBuildingFromCoord(fromX, fromY);
+    bytes32 to = BuildingLogic.getBuildingFromCoord(toX, toY);
+    transferERC20(itemType, from, to, amount);
+  }
+
+  function transferERC20(bytes16 itemType, bytes32 from, bytes32 to, uint128 amount) private onlyController(from) {
     ContainerLogic._transfer(itemType, from, to, amount);
   }
 
-  // function transferERC721
+  function transferERC721(
+    uint32 fromX,
+    uint32 fromY,
+    uint32 toX,
+    uint32 toY,
+    bytes32 erc721
+  ) public onlyAdjacentCoords(fromX, fromY, toX, toY) {
+    bytes32 from = BuildingLogic.getBuildingFromCoord(fromX, fromY);
+    bytes32 to = BuildingLogic.getBuildingFromCoord(toX, toY);
+    transferERC721(from, to, erc721);
+  }
+
+  function transferERC721(bytes32 from, bytes32 to, bytes32 erc721) private onlyController(from) {
+    ContainerLogic._transfer(from, to, erc721);
+  }
 }
